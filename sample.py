5c5ef3d7e5707080e0040c81c9fbd53ed1d53e14
Abin Chandra Babu <abincbabu05@gmail.com>
2024-01-26 12:58:09 +0530

    update

--- sample.py
+++ sample.py
@@ -299,34 +299,7 @@ def create_map_user(path_map_user, column_map_user):
                     pass
     return pd.DataFrame(data_list,columns=column_map_user)
 
-# def create_top_transaction(path_top_transaction, column_top_transaction):
-#     data_list=[]
-#     top_trans = os.listdir(path_top_transaction)
-#
-#     for state in top_trans:
-#         p_state = os.path.join(path_top_transaction, state)
-#         year_list = os.listdir(p_state)
-#
-#         for year in year_list:
-#             p_year = os.path.join(p_state, year)
-#             quarter_list = os.listdir(p_year)
-#
-#             for quarter in quarter_list:
-#                 p_quarter = os.path.join(p_year, quarter)
-#                 data_file = open(p_quarter, 'r')
-#                 data = json.load(data_file)
-#
-#             try:
-#                 for value in data['data']['pincodes']:
-#                     p_code = value['entityName']
-#                     trans_count = value['metric']['count']
-#                     trans_amount = value['metric']['amount']
-#                     row_data = [state,year,int(quarter.strip('.json')),p_code,trans_count,trans_amount]
-#                     data_list.append(row_data)
-#             except:
-#                 pass
-#
-#     return pd.DataFrame(data_list,columns=column_top_transaction)
+
 
 def create_top_transaction(path5):
     top_trans_list = os.listdir(path5)
@@ -384,123 +357,12 @@ def create_top_user(path_top_user, column_top_user):
 
     return pd.DataFrame(data_list,columns=column_top_user)
 
-# # # Create DataFrames
-# path_transaction = "pulse/data/aggregated/transaction/country/india/state/"
-# column_names_transaction = ['State', 'Year', 'Quarter', 'Transaction_type', 'Transaction_count', 'Transaction_amount']
-# agg_trans = create_aggregated_tansaction (path_transaction, column_names_transaction)
-# st.write("Aggregated_Trans")
-# st.dataframe(agg_trans)
-
-# # Bar chart
-# fig, ax = plt.subplots(figsize=(12, 6))
-# state_totals = agg_trans.groupby('State')['Transaction_amount'].sum()
-# state_totals.plot(kind='bar', ax=ax, color='skyblue')
-# ax.set_xlabel("State")
-# ax.set_ylabel("Total Transaction Amount")
-# ax.set_title("Total Transaction Amount by State")
-# # Display the plot using Streamlit
-# st.pyplot(fig)
-
-# # for index, row in agg_trans.iterrows():
-# #        # insert_into_agg_trans(row)
-# #
-# #         try:
-# #             insert_into_agg_trans(row)
-# #         except Exception as e:
-# #             print(f"Error inserting row: {e}")
-# # print("success")
-
-# path_user = "pulse/data/aggregated/user/country/india/state/"
-# column_names_user = ['State', 'Year', 'Quarter', 'Brands', 'Count','Percentage']
-# agg_user = create_aggregated_user (path_user, column_names_user)
-# # Display the DataFrame
-# st.write("Aggregated_User")
-# st.dataframe(agg_user)
-# # Bar chart 
-# fig, ax = plt.subplots(figsize=(12, 6))
-# colors = sns.color_palette("husl", n_colors=len(agg_user['Brands'].unique()))
-# for brand, color in zip(agg_user['Brands'].unique(), colors):
-#     subset = agg_user[agg_user['Brands'] == brand]
-#     ax.bar(subset['State'], subset['Count'], label=brand, color=color)
-
-# ax.set_xlabel("State")
-# ax.set_ylabel("User Count")
-# ax.set_title("User Count by State and Brands")
-# ax.legend(title='Brands', bbox_to_anchor=(1.05, 1), loc='upper left')
-# ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
-# st.pyplot(fig)
-# # for index, row in agg_user.iterrows():
-# #        insert_into_agg_user(row)
-# # print("success")
-# #
-# #
-# path_map_transaction = "pulse/data/map/transaction/hover/country/india/state/"
-# column_names_transaction = ['State','Year','Quarter','Amount','District','Count']
-# map_trans = create_map_transaction(path_map_transaction, column_names_transaction)
-# # Display the DataFrame
-# st.write("Map_Trans")
-# st.dataframe(map_trans)
-# # Plotting a bar chart
-# fig, ax = plt.subplots()
-# ax.bar(map_trans['State'], map_trans['Amount'])
-# ax.set_xlabel('State')
-# ax.set_ylabel('Transaction Amount')
-# ax.set_title('Transaction Amount by State in India')
-# ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
-# st.pyplot(fig)
-
-# # # for index, row in map_trans.iterrows():
-# # #        insert_into_map_trans(row)
-# # # print("success")
-# #
-# path_map_user = "pulse/data/map/user/hover/country/india/state/"
-# column_map_user = ['State','Year','Quarter','District','RegisteredUser','AppOpens']
-# map_user = create_map_user(path_map_user, column_map_user)
-# st.write("Map_User")
-# st.write(map_user)
-# # Plotting a bar chart
-# fig, ax = plt.subplots()
-# ax.bar(map_user['State'], map_user['RegisteredUser'])
-# ax.set_xlabel('State')
-# ax.set_ylabel('Registered Users')
-# ax.set_title('Registered Users by State in India')
-# ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
-# st.pyplot(fig)
-
-# # for index, row in map_user.iterrows():
-# #        insert_into_map_user(row)
-# # print("success")
-# # column_top_transaction = ['State','Year','Quarter','Pincode','Transaction_count','Transaction_amount']
-
-# path_top_transaction = "pulse/data/top/transaction/country/india/state/"
-# top_trans = create_top_transaction(path_top_transaction)
-# # Plotting a bar chart
-# st.write("TopTransaction")
-# st.write(top_trans)
-# fig, ax = plt.subplots()
-# ax.bar(top_trans['State'], top_trans['Transaction_amount'])
-# ax.set_xlabel('State')
-# ax.set_ylabel('Transaction Amount')
-# ax.set_title('Top Transactions by State in India')
-# ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
-# st.pyplot(fig)
 
 path_top_user = "pulse/data/top/user/country/india/state/"
 column_top_user = ['State','Year','Quarter','z','RegisteredUsers']
 top_user = create_top_user(path_top_user, column_top_user)
 st.write("Top User")
 st.write(top_user)
-#
-# for index, row in top_user.iterrows():
-#        insert_into_top_user(row)
-# print("success")
-# fig, ax = plt.subplots(figsize=(12, 6))
-# state_totals = top_user.groupby('State')['RegisteredUsers'].sum()
-# state_totals.plot(kind='bar', ax=ax, color='skyblue')
-# ax.set_xlabel("State")
-# ax.set_ylabel("RegisteredUsers")
-# ax.set_title("Pincode")
-# st.pyplot(fig)
 
 # Create a colorful bar chart
 fig, ax = plt.subplots(figsize=(12, 6))
@@ -520,19 +382,4 @@ ax.set_title("Registered Users by State with Colorful Bars")
 
 # Display the colorful bar chart in Streamlit
 st.pyplot(fig)
-
-
-# path5="pulse/data/top/transaction/country/india/state/"
-# top_trans = create_top_transaction(path5)
-# st.dataframe(top_trans)
-
-
-# print(top_trans)
-# for index, row in top_trans.iterrows():
-#     insert_into_top_trans(row)
-# print("success")
-# Assuming top_trans is a pandas DataFrame with columns 'State' and 'Transaction_amount'
-# Set up subplots
-# Set up subplots
-# Bar chart
 
